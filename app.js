b305704f1d564e81979dcb91aa218da3f131451b
williamsmichael <willia32@hotmail.com>
2016-07-19 01:12:06 -0700

    Completion of Level 2: Built-in Directives - Filters, tabs, more controllers.

--- app.js
+++ app.js
@@ -3,28 +3,56 @@
 
 (function(){
 	var app = angular.module('store', []);
+
+	// controllers
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	});
+
+	// gems
 	var gems = [
 		{
-			name: 'Dodecahedron', price: 2.95, description: 'Some gems have hidden qualities beyond their luster, beyond their shine. Dodeca is one of those gems', canPurchase: true,
+			name: 'Dodecahedron', 
+			price: 2.95, 
+			description: 'Some gems have hidden qualities beyond their luster, beyond their shine. Dodeca is one of those gems', 
+			canPurchase: true, 
+			images: {
+				full: 'images/gem-full.png',
+				thumb: 'images/gem-thumb.png',
+			},
 		},
 		{
-			name: 'Pentagonal Gem', price: 5.95, description: '. . .', canPurchase: true,
+			name: 'Pentagonal Gem', 
+			price: 5.95, 
+			description: '. . .', 
+			canPurchase: true,
+			images: {
+				full: 'images/gem2-full.jpg',
+				thumb: 'images/gem2-thumb.jpg',
+			},
 
 		},
 		{ 
 			name: 'Azurite', price: 2.95, description: '. . .', canPurchase: true,
 		},
     	{ 
-    		name: 'Bloodstone', price: 5.95, description: '. . .', canPurchase: true,
+    		name: 'Bloodstone', price: 2, description: '. . .', canPurchase: true,
     	},
     	{ 
     		name: 'Zircon', price: 3.95, description: '. . .', canPurchase: true, 
     	}
 	];
 
-	app.controller('StoreController', function(){
-		this.products = gems;
-	});
 })();
 
 
